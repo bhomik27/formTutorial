7cfd4e25601b894b802f57c50bcd34f7292adccc
bhomik27 <bhomikmaheshwari27@gmail.com>
2023-07-16 02:02:39 +0530

    added userDetails as object in  local storage

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 function saveToLocalStorage(event) {
     event.preventDefault();
+    
 
     let name = event.target.name.value;
     let email = event.target.email.value;
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
+    // localStorage.setItem('name', name);
+    // localStorage.setItem('email', email);
+
+
+    let userDetails = {
+        name , 
+        email
+    }
+
+    let userDetails_serialized = JSON.stringify(userDetails);
+
+    localStorage.setItem("userDetail", userDetails_serialized);
 
 }
